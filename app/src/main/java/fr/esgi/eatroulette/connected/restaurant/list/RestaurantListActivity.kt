54a44323db416ffb5fc6df205e09b73bd81305d6
Gwendal <gwendou95@hotmail.fr>
2020-12-27 17:06:03 +0100

    ✨ Refresh list on scroll down

--- app/src/main/java/fr/esgi/eatroulette/connected/restaurant/list/RestaurantListActivity.kt
+++ app/src/main/java/fr/esgi/eatroulette/connected/restaurant/list/RestaurantListActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import fr.esgi.eatroulette.R
@@ -11,12 +12,12 @@ import fr.esgi.eatroulette.connected.restaurant.Restaurant
 import fr.esgi.eatroulette.connected.restaurant.Type
 import fr.esgi.eatroulette.connected.restaurant.detail.RestaurantDetailActivity
 import fr.esgi.eatroulette.infrastructure.ApiRepository
-import fr.esgi.eatroulette.infrastructure.User
 import kotlinx.android.synthetic.main.activity_restaurant_detail.*
 import kotlinx.android.synthetic.main.activity_restaurant_list.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+
 
 class RestaurantListActivity : AppCompatActivity(),
     RestaurantViewHolder.OnRestaurantClickedListener {
@@ -35,25 +36,39 @@ class RestaurantListActivity : AppCompatActivity(),
 
     )
 
-    private var restaurants:List<Restaurant>? = listOf(
-        Restaurant("Enjoy Sushi",  typeList,"3 rue Victor Hugo", "PARIS", "75008"),
+    private var restaurants: List<Restaurant>? = listOf(
+        Restaurant("Enjoy Sushi", typeList, "3 rue Victor Hugo", "PARIS", "75008"),
         Restaurant("Mc Donald", typeList, "3 rue Victor Hugo", "PARIS", "75008"),
-        Restaurant("Burger King", typeList,"3 rue Victor Hugo", "PARIS", "75008"),
-        Restaurant("Five Guys", typeList,"3 rue Victor Hugo", "PARIS", "75008"),
-        Restaurant("Crepe Party",typeList, "3 rue Victor Hugo", "PARIS", "75008"),
-        Restaurant("Pepper Joy",typeList, "3 rue Victor Hugo", "PARIS", "75008"),
-        Restaurant("EatRoulette",typeList, "3 rue Victor Hugo", "PARIS", "75008"),
-        Restaurant("Trionfo",typeList, "3 rue Victor Hugo", "PARIS", "75008"),
-        Restaurant("Flora Danica",typeList, "3 rue Victor Hugo", "PARIS", "75008"),
-        Restaurant("L'ins Temps Dej",typeList, "3 rue Victor Hugo", "PARIS", "75008"),
-        Restaurant("VeganMiam",typeList, "3 rue Victor Hugo", "PARIS", "75008"),
+        Restaurant("Burger King", typeList, "3 rue Victor Hugo", "PARIS", "75008"),
+        Restaurant("Five Guys", typeList, "3 rue Victor Hugo", "PARIS", "75008"),
+        Restaurant("Crepe Party", typeList, "3 rue Victor Hugo", "PARIS", "75008"),
+        Restaurant("Pepper Joy", typeList, "3 rue Victor Hugo", "PARIS", "75008"),
+        Restaurant("EatRoulette", typeList, "3 rue Victor Hugo", "PARIS", "75008"),
+        Restaurant("Trionfo", typeList, "3 rue Victor Hugo", "PARIS", "75008"),
+        Restaurant("Flora Danica", typeList, "3 rue Victor Hugo", "PARIS", "75008"),
+        Restaurant("L'ins Temps Dej", typeList, "3 rue Victor Hugo", "PARIS", "75008"),
+        Restaurant("VeganMiam", typeList, "3 rue Victor Hugo", "PARIS", "75008"),
     )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_restaurant_list)
 
-        ApiRepository.retrieveAllRestaurant(object: Callback<List<Restaurant>> {
+        loadData()
+
+        pullToRefresh.setOnRefreshListener {
+            loadData()
+            pullToRefresh.isRefreshing = false
+        }
+
+    }
+
+    override fun onRestaurantClicked(restaurant: Restaurant?) {
+        restaurant?.let { RestaurantDetailActivity.navigateTo(this, it) }
+    }
+
+    private fun loadData(){
+        ApiRepository.retrieveAllRestaurant(object : Callback<List<Restaurant>> {
             override fun onFailure(call: Call<List<Restaurant>>, t: Throwable) {
                 Log.d("toto", "Error : ${t.message}")
             }
@@ -66,16 +81,12 @@ class RestaurantListActivity : AppCompatActivity(),
                 Log.d("toto", "Code ${response.code()}, Restaurants = ${response.body()}")
                 restaurantList?.apply {
                     layoutManager = LinearLayoutManager(this@RestaurantListActivity)
-                    adapter = restaurants?.let { RestaurantAdapter(it, this@RestaurantListActivity) }
+                    adapter =
+                        restaurants?.let { RestaurantAdapter(it, this@RestaurantListActivity) }
                 }
+                loader?.visibility = View.INVISIBLE
+                restaurantList?.visibility = View.VISIBLE
             }
-
         })
-
-
-    }
-
-    override fun onRestaurantClicked(restaurant: Restaurant?) {
-        restaurant?.let { RestaurantDetailActivity.navigateTo(this, it) }
     }
 }
