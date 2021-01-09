2ab9941df735d246bc44fbba69bb1809b6cf327f
LAymeric <aymeric.larvet@gmail.com>
2021-01-09 22:01:00 +0100

    building register page + title on login page + navigation

--- app/src/main/java/fr/esgi/eatroulette/MainActivity.kt
+++ app/src/main/java/fr/esgi/eatroulette/MainActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import fr.esgi.eatroulette.login.LoginActivity
+import fr.esgi.eatroulette.register.RegisterActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -20,7 +21,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        registerBtn?.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            this.startActivity(intent)
+        }
         connectBtn?.setOnClickListener {
             val intent = Intent(this, LoginActivity::class.java)
             this.startActivity(intent)
---
+++ app/src/main/java/fr/esgi/eatroulette/register/RegisterActivity.kt
@@ -0,0 +1,16 @@
+package fr.esgi.eatroulette.register
+
+import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import fr.esgi.eatroulette.R
+
+class RegisterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+    }
+}
